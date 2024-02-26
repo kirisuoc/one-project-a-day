da9ed4b2bf8591fa52f03223c37ec8109cc4f85b
kirisuoc <erikcousillas@icloud.com>
2024-02-26 23:28:10 +0000

    finished

---
+++ Toast message/script.js
@@ -0,0 +1,7 @@
+function triggerToast() {
+    const toast = document.getElementById('toast');
+    toast.classList.add('show');
+    setTimeout(() => {
+        toast.classList.remove('show');
+    }, 3000);
+}
