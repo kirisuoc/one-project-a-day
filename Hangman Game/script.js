b216438078d03b616af741d738b611dde750232e
kirisuoc <erikcousillas@icloud.com>
2024-06-09 15:29:38 +0100

    Summary

--- Hangman Game/script.js
+++ Hangman Game/script.js
@@ -1,103 +1,93 @@
-const {
-    gsap: { registerPlugin, set, to, timeline },
-    MorphSVGPlugin,
-    Draggable } =
-    window;
-  registerPlugin(MorphSVGPlugin);
+let newWord
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P',
+                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-  // Used to calculate distance of "tug"
-  let startX;
-  let startY;
+// Añadir letras del abecedario
+const keys = document.querySelector('.keys')
 
-  const AUDIO = {
-    CLICK: new Audio('https://assets.codepen.io/605876/click.mp3')
-  };
+alphabet.forEach(letter => {
+  let alphabetLetter = document.createElement('span')
+  alphabetLetter.innerHTML = letter
+  keys.appendChild(alphabetLetter)
+})
 
-  const STATE = {
-    ON: false
-  };
 
-  const CORD_DURATION = 0.1;
+fetch('./words.json')
+  .then(response => response.json())
+  .then(data => {
+    // Seleccionar una palabra aleatoria de la lista del archivo JSON
+    const randomIndex = Math.floor(Math.random() * data.length)
+    newWord = data[randomIndex].palabra.toUpperCase()
 
-  const CORDS = document.querySelectorAll('.toggle-scene__cord');
-  const HIT = document.querySelector('.toggle-scene__hit-spot');
-  const DUMMY = document.querySelector('.toggle-scene__dummy-cord');
-  const DUMMY_CORD = document.querySelector('.toggle-scene__dummy-cord line');
-  const PROXY = document.createElement('div');
-  // set init position
-  const ENDX = DUMMY_CORD.getAttribute('x2');
-  const ENDY = DUMMY_CORD.getAttribute('y2');
-  const RESET = () => {
-    set(PROXY, {
-      x: ENDX,
-      y: ENDY
-    });
+    // Dividir la palabra en un array de letras
+    letters = newWord.split('')
 
-  };
-
-  RESET();
-
-  const CORD_TL = timeline({
-    paused: true,
-    onStart: () => {
-      STATE.ON = !STATE.ON;
-      set(document.documentElement, { '--on': STATE.ON ? 1 : 0 });
-      set([DUMMY, HIT], { display: 'none' });
-      set(CORDS[0], { display: 'block' });
-      AUDIO.CLICK.play();
-    },
-    onComplete: () => {
-      set([DUMMY, HIT], { display: 'block' });
-      set(CORDS[0], { display: 'none' });
-      RESET();
+    // Llamar a la función para actualizar la palabra en el DOM
+    updateWord(letters)
+    // Llamar a la función para actualizar la pista si la hay
+    if (data[randomIndex].definicion) {
+      updateHint(data, randomIndex)
     }
+  })
+  .catch(error => console.log('Error: ', error))
+
+
+
+  
+const wordToGuess = document.getElementById('word-to-guess')
+function updateWord(letters) {
+  // Limpiar el contenido anterior
+  wordToGuess.innerHTML = ''
+
+  // Crear y agregar los spans para cada letra
+  letters.forEach(letter => {
+    let newLetterEl = document.createElement('span')
+    newLetterEl.innerHTML = letter
+    newLetterEl.classList.add('hidden')
+    newLetterEl.style.color = 'transparent'
+    wordToGuess.appendChild(newLetterEl)
   });
+}
+
+const hintContainer = document.querySelector('.central-container')
+function updateHint(data, randomIndex) {
+
+  let newHint = `<p class="hint">
+                  <strong>Hint: </strong>
+                  ${data[randomIndex].definicion}
+                </p>`
+
+  // Inserta el newHint en el elemento hintContainer
+  hintContainer.insertAdjacentHTML('afterbegin', newHint)
+}
+
+Array.from(keys.children).forEach(letter => {
+  letter.addEventListener('click', () => {
+    const clickedLetter = letter.innerHTML.trim() // Obtiene el contenido del span sin espacios en blanco
+    let errors = 0
+
+    letter.classList.add('selected')
+
+    Array.from(wordToGuess.children).forEach(wordLetter => {
+      if(wordLetter.innerHTML.trim() === clickedLetter) {
+        wordLetter.style.color = 'black'
+        wordLetter.classList.remove('hidden')
+      }
+    })
 
 
-  for (let i = 1; i < CORDS.length; i++) {
-    CORD_TL.add(
-      to(CORDS[0], {
-        morphSVG: CORDS[i],
-        duration: CORD_DURATION,
-        repeat: 1,
-        yoyo: true
-      }));
+  })
+})
 
 
-  }
 
-  Draggable.create(PROXY, {
-    trigger: HIT,
-    type: 'x,y',
-    onPress: e => {
-      startX = e.x;
-      startY = e.y;
-    },
-    onDrag: function () {
-      set(DUMMY_CORD, {
-        attr: {
-          x2: this.x,
-          y2: this.y
-        }
-      });
-
-
-    },
-    onRelease: function (e) {
-      const DISTX = Math.abs(e.x - startX);
-      const DISTY = Math.abs(e.y - startY);
-      const TRAVELLED = Math.sqrt(DISTX * DISTX + DISTY * DISTY);
-      to(DUMMY_CORD, {
-        attr: { x2: ENDX, y2: ENDY },
-        duration: CORD_DURATION,
-        onComplete: () => {
-          if (TRAVELLED > 50) {
-            CORD_TL.restart();
-          } else {
-            RESET();
-          }
-        }
-      });
-
+/* function checkWord() {
+  Array.from(wordToGuess.children).forEach(letter => {
+    for(i = 0; i < alphabet.length; i++ )
+      if(letter.innerHTML === alphabet[i]) {
+        letter.style.color = 'black'
     }
-  });
+  })
+})
+ */
