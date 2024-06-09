b93e408cd905224c867f85d2f5a8ab5d7da9f332
kirisuoc <erikcousillas@icloud.com>
2024-06-09 18:37:18 +0100

    Update
    copia seguridad

--- Hangman Game/script.js
+++ Hangman Game/script.js
@@ -5,37 +5,38 @@ const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 
 // Añadir letras del abecedario
 const keys = document.querySelector('.keys')
-
-alphabet.forEach(letter => {
-  let alphabetLetter = document.createElement('span')
-  alphabetLetter.innerHTML = letter
-  keys.appendChild(alphabetLetter)
-})
-
-
-fetch('./words.json')
-  .then(response => response.json())
-  .then(data => {
-    // Seleccionar una palabra aleatoria de la lista del archivo JSON
-    const randomIndex = Math.floor(Math.random() * data.length)
-    newWord = data[randomIndex].palabra.toUpperCase()
-
-    // Dividir la palabra en un array de letras
-    letters = newWord.split('')
-
-    // Llamar a la función para actualizar la palabra en el DOM
-    updateWord(letters)
-    // Llamar a la función para actualizar la pista si la hay
-    if (data[randomIndex].definicion) {
-      updateHint(data, randomIndex)
-    }
-  })
-  .catch(error => console.log('Error: ', error))
-
-
-
-  
 const wordToGuess = document.getElementById('word-to-guess')
+const hintContainer = document.querySelector('.central-container')
+
+// Función para añadir letras del abecedario
+function addAlphabetLetters() {
+  alphabet.forEach(letter => {
+    let alphabetLetter = document.createElement('span')
+    alphabetLetter.innerHTML = letter
+    keys.appendChild(alphabetLetter)
+    
+    // Event listener para las letras del abecedario
+    alphabetLetter.addEventListener('click', () => {
+      handleLetterClick(alphabetLetter)
+    })
+  })
+}
+
+
+// Función para obtener una palabra aleatoria y actualizar la pantalla
+function getRandomWordAndDisplay(data) {
+  const randomIndex = Math.floor(Math.random() * data.length)
+  newWord = data[randomIndex].palabra.toUpperCase()
+  const letters = newWord.split('')
+
+  updateWord(letters)
+
+  if (data[randomIndex].definicion) {
+    updateHint(data[randomIndex].definicion)
+  }
+}
+
+// Función para actualizar la palabra en el DOM
 function updateWord(letters) {
   // Limpiar el contenido anterior
   wordToGuess.innerHTML = ''
@@ -47,38 +48,49 @@ function updateWord(letters) {
     newLetterEl.classList.add('hidden')
     newLetterEl.style.color = 'transparent'
     wordToGuess.appendChild(newLetterEl)
-  });
+});
 }
-
-const hintContainer = document.querySelector('.central-container')
-function updateHint(data, randomIndex) {
-
+// Función para actualizar la pista en el DOM
+function updateHint(hint) {
   let newHint = `<p class="hint">
                   <strong>Hint: </strong>
-                  ${data[randomIndex].definicion}
+                  ${hint}
                 </p>`
 
   // Inserta el newHint en el elemento hintContainer
   hintContainer.insertAdjacentHTML('afterbegin', newHint)
 }
 
-Array.from(keys.children).forEach(letter => {
-  letter.addEventListener('click', () => {
-    const clickedLetter = letter.innerHTML.trim() // Obtiene el contenido del span sin espacios en blanco
-    let errors = 0
+// Función para manejar el evento click en las letras del abecedario
+function handleLetterClick(letter) {
+  const clickedLetter = letter.innerHTML.trim() // Obtiene el contenido del span sin espacios en blanco
+  letter.classList.add('selected')
 
-    letter.classList.add('selected')
-
-    Array.from(wordToGuess.children).forEach(wordLetter => {
-      if(wordLetter.innerHTML.trim() === clickedLetter) {
-        wordLetter.style.color = 'black'
-        wordLetter.classList.remove('hidden')
-      }
-    })
-
-
+  Array.from(wordToGuess.children).forEach(wordLetter => {
+    if(wordLetter.innerHTML.trim() === clickedLetter) {
+      wordLetter.style.color = 'black'
+      wordLetter.classList.remove('hidden')
+    }
   })
-})
+}
+
+
+
+// Llamadas a funciones principales después de cargar el JSON
+fetch('./words.json')
+  .then(response => response.json())
+  .then(data => {
+    addAlphabetLetters()
+    getRandomWordAndDisplay(data)
+  })
+  .catch(error => console.log('Error: ', error))
+
+
+
+
+
+
+
 
 
 
